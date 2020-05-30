43d8e101ecd7a2eb97da9d015ba6e15af53a216d
georgiev-ventsi <v.g.al.hq@gmail.com>
2020-05-30 03:42:12 +0300

    add two sliders

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+//  first slider
  
+ $('.slider-one')
+ .not("slick-initialized")
+ .slick({
+     autoplay: true,
+     autoplaySpeed: 2500,
+     dots: true,
+     prevArrow: ".site-slider .slider-btn .prev",
+     nextArrow: ".site-slider .slider-btn .next"
+ })
+
+
+//  second slider
+ $('.slider-two')
+ .not("slick-initialized")
+ .slick({
+     prevArrow: ".site-slider-two .prev",
+     nextArrow: ".site-slider-two .next",
+     slidesToShow: 5,
+     slidesToScroll: 1,
+     autoplay: true,
+     autoplaySpeed: 2000
+ })
+
